f8fa5345adeb747fe2c394b9b92f93a100158765
hamza <hamzash863@gmail.com>
2022-04-30 12:23:26 +0300

    get data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robot";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => this.setState({ robots: users }));
   }
 
   onSearchChange = (event) => {
@@ -19,13 +24,19 @@ class App extends Component {
     const filteredRobots = this.state.robots.filter((robot) => {
       return robot.name.toLowerCase().includes(this.state.searchfield);
     });
-    return (
-      <div>
-        <h1 className="text-6xl text-red-800 text-center pt-2">Robo Friend</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      <h1 className="text-6xl text-red-800 text-center p-4">Loading...</h1>;
+    } else {
+      return (
+        <div>
+          <h1 className="text-6xl text-red-800 text-center pt-2">
+            Robo Friend
+          </h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
